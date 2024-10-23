d4f9fd36eff21de9d0f02e7f747aeb58f3f4941e
Kamron Khamrakulov <khamrakulovkamron@gmail.com>
2024-10-24 02:59:18 +0500

    added auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,74 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcryptjs';
+import { User } from 'src/users/users.model';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(userDto: CreateUserDto) {
+    const user = await this.validate(userDto);
+    return this.generateToken(user);
+  }
+
+  async register(userDto: CreateUserDto) {
+    const candidate = await this.usersService.getUserByEmail(
+      userDto.email,
+    );
+
+    if (candidate) {
+      throw new HttpException(
+        'user with email already exists',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const hashPassword = await bcrypt.hash(userDto.password, 5);
+    const user = await this.usersService.createUser({
+      ...userDto,
+      password: hashPassword,
+    });
+
+    return this.generateToken(user);
+  }
+
+  async generateToken(user: User) {
+    const payload = {
+      email: user.email,
+      id: user.id,
+      roles: user.roles,
+    };
+
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
+
+  private async validate(userDto: CreateUserDto) {
+    const user = await this.usersService.getUserByEmail(
+      userDto.email,
+    );
+    const passwordEquals = await bcrypt.compare(
+      userDto.password,
+      user.password,
+    );
+
+    if (user && passwordEquals) {
+      return user;
+    }
+    throw new UnauthorizedException({
+      message: 'Incorrect email or password',
+    });
+  }
+}
